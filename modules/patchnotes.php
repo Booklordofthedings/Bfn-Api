1f21877edef4f161cdaedd704d12dd90737d2390
Book <jannis.v.hagen@gmail.com>
2023-04-09 19:51:32 +0200

    more

--- modules/patchnotes.php
+++ modules/patchnotes.php
@@ -7,24 +7,26 @@
 
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
         <link rel="stylesheet" href="theme.css" >
+        <script src="script.js"></script>
 
-        
     </head>
     <body class="bg-secondary text-primary">
-        
-    <!--include the modular header.php as a html file--->
-    <?php include $_SERVER['DOCUMENT_ROOT']."/header.php"?>
-
-    <div class="container">
+      <?php include $_SERVER['DOCUMENT_ROOT']."/header.php"; ?>
+      <div class="container">
         <br>
-        <div class="card bg-dark">
-            <div class="card-header"> <h3>About this website<h3> </div>
-            <div class="card-body">
-                
-            </div>
-        </div>
-    </div>
 
-    <?php include $_SERVER['DOCUMENT_ROOT']."/modules/bootstrap_footer.php"; ?>
+        <div class="card bg-dark">
+        <h5 class="card-header">Version 1.0</h5>
+        <div class="card-body">
+          <ul class="list-unstyled card-text">
+            <li>Put page online</li>
+            <li>Added basic about sections</li>
+            </ul>
+        </div>
+        </div>
+
+      <?php include $_SERVER['DOCUMENT_ROOT']."/modules/footer.php"; ?>
+      </div>
+      <?php include $_SERVER['DOCUMENT_ROOT']."/modules/bootstrap_footer.php"; ?>
     </body>
 </html>
