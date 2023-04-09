6880e4b4ed3347ca9dc2f7ef614f4d60a5a21331
Book <jannis.v.hagen@gmail.com>
2023-04-09 18:43:08 +0200

    change header bar link ways

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-dark bg-primary navbar-expand-sm">
-        <a class="navbar-brand" href="https://electric-sli.de">
+        <a class="navbar-brand" href="/">
           <img src="res/icon.png" width="30" height="30" class="d-inline-block align-top" alt="">
           The Funderdome
         </a>
@@ -12,13 +12,13 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav">
             <li class="nav-item active">
-              <a class="nav-link" href="index.php">Search</a>
+              <a class="nav-link" href="/">Search</a>
             </li>
             <li class="nav-item disabled">
               <a class="nav-link">Compare</a>
             </li>
             <li class="nav-item active">
-              <a class="nav-link" href="about.php">About & Guide</a>
+              <a class="nav-link" href="/about">About & Guide</a>
             </li>
 
           </ul>
