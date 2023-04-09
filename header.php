cc492a9ebecbf6b6ec0a01b960a2c52a21892506
Book <jannis.v.hagen@gmail.com>
2023-04-09 18:40:04 +0200

    get started with modulization

---
+++ header.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-dark bg-primary navbar-expand-sm">
+        <a class="navbar-brand" href="https://electric-sli.de">
+          <img src="res/icon.png" width="30" height="30" class="d-inline-block align-top" alt="">
+          The Funderdome
+        </a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="index.php">Search</a>
+            </li>
+            <li class="nav-item disabled">
+              <a class="nav-link">Compare</a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="about.php">About & Guide</a>
+            </li>
+
+          </ul>
+        </div>
+      </nav>
