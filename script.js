3309dbc7f641a64d44b14c5cab84a4c7f7baa8aa
Book <jannis.v.hagen@gmail.com>
2023-04-15 21:37:36 +0200

    fix

--- script.js
+++ script.js
@@ -6,6 +6,6 @@ function RemoveChangelog() {
 
 //Edit the bootstrap selector text on select
 function EditSelector(event) {
-    var selector = document.getElementById("SearchDropdown);
+    var selector = document.getElementById("SearchDropdown");
     selector.innerHTML = event.target.innerHTML;
 }
