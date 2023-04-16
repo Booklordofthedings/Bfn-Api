9b49bf16979315812f77a4c60eaeccc0939238f8
Book <jannis.v.hagen@gmail.com>
2023-04-16 15:14:41 +0200

    github link

--- modules/footer.php
+++ modules/footer.php
@@ -1,1 +1,6 @@
-<div class="card bg-dark"> <div class="card-body" style="text-align:center">Plants vs Zombies and all associated Images and characters are property of Electronic Arts, which are in not affiliated with this website and its contents</div></div>
+<div class="card bg-dark"> 
+    <div class="card-body" style="text-align:center">
+        Plants vs Zombies and all associated Images and characters are property of Electronic Arts, which are in not affiliated with this website and its contents. <br>
+        Most code from this website is open source and can be viewed and contributed to on <a href="https://github.com/Booklordofthedings/Bfn-Api">Github</a>
+    </div>
+</div>
